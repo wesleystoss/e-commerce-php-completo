3f340245b9e57b71f93ef8c6645a3ceeb5322acb
Wesley Santos <wesleyrogerio77@gmail.com>
2025-06-23 16:25:08 -0300

    feat: adição do carregamento de variáveis de ambiente a partir do arquivo .env

--- src/public/index.php
+++ src/public/index.php
@@ -2,6 +2,18 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+// Carregar variáveis de ambiente
+$envFile = __DIR__ . '/../.env';
+if (file_exists($envFile)) {
+    $lines = file($envFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        if (strpos($line, '=') !== false && strpos($line, '#') !== 0) {
+            list($key, $value) = explode('=', $line, 2);
+            $_ENV[trim($key)] = trim($value);
+        }
+    }
+}
+
 // Iniciar sessão
 session_start();
 
