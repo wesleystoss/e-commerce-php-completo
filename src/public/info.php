42d991e8f1782475c4aa210e61d53a6a4d44e3a9
Wesley Santos <wesleyrogerio77@gmail.com>
2025-06-24 22:05:54 -0300

    refactor: simplificação das regras de reescrita no .htaccess, redirecionando todas as requisições para index.php e remoção de arquivos de teste obsoletos

--- src/public/info.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "<h1>Teste de Arquivo Físico</h1>";
-echo "<p>Se você está vendo esta página, arquivos físicos estão sendo servidos corretamente.</p>";
-echo "<p>Data e hora: " . date('Y-m-d H:i:s') . "</p>";
-echo "<p>Servidor: " . $_SERVER['SERVER_NAME'] . "</p>";
-echo "<p>Document Root: " . $_SERVER['DOCUMENT_ROOT'] . "</p>";
-echo "<p>Script Filename: " . $_SERVER['SCRIPT_FILENAME'] . "</p>";
-echo "<p>Request URI: " . $_SERVER['REQUEST_URI'] . "</p>";
-?> 
